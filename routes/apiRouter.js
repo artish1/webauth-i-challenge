4cc7c59749f6fdc168447f3642763165c924a654
Mark Artishuk <markyshuk@gmail.com>
2019-12-16 12:46:11 -0800

    Added restricted middleware function for verifying users more easily and implemented get users function

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -51,6 +51,37 @@ router.post("/login", (req, res) => {
   }
 });
 
-router.get("/users", (req, res) => {});
+router.get("/users", restricted, (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err);
+      console.log("Error getting users");
+    });
+});
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          req.user = user;
+          next();
+        } else {
+          res.status(401).json({ message: "Invalid Credentials" });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        res.status(500).json({ message: "Unexpected error" });
+      });
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
+}
 
 module.exports = router;
