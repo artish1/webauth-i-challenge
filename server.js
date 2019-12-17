b864d302c4b1bd832213f6a898900a4a155ad4c5
Mark Artishuk <markyshuk@gmail.com>
2019-12-17 12:12:57 -0800

    Added express session configurations

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ const express = require("express");
 const apiRouter = require("./routes/apiRouter");
 const server = express();
 
+const session = require("express-session");
+
+const sessionConfig = {
+  name: "sessionid",
+  secret: "My Fake Secret",
+  cookie: {
+    maxAge: 1000 * 60 * 30, // 1/2 hour
+    secure: false,
+    httpOnly: true
+  },
+  resave: false,
+  saveUninitialized: false
+};
+
 server.use(express.json());
+server.use(session(sessionConfig));
+
 server.use("/api", apiRouter);
 
 module.exports = server;
